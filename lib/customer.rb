1cf928c2a35981ebf228773cd81007597041b865
Avijit Lodh <avijitklodh@gmail.com>
2019-08-09 09:58:10 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,20 @@
 class Customer
+  attr_reader :Customers
+  
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name
+    @age = age 
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end 
+  
+  
   
 end
+
+
